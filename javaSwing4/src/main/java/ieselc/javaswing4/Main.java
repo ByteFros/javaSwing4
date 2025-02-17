f3915f61db743d2cdd2ecd9863696c35919bcebc
Debi Doce <debi12@DESKTOP-L23S58C.home>
2025-02-17 19:04:35 +0100

    renamed packages

--- javaSwing4/src/main/java/ieselc/javaswing4/Main.java
+++ javaSwing4/src/main/java/ieselc/javaswing4/Main.java
@@ -1,7 +1,11 @@
 package ieselc.javaswing4;
 
+import ieselc.javaswing4.gui.frames.JDatabaseFilePathFrame;
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame dbLocationFrame = new JDatabaseFilePathFrame();
+        dbLocationFrame.setVisible(true);
     }
 }
